5cf24136ee227a63555b7ef528d2fee1612ef727
jungchuen <50093044+jungchuen@users.noreply.github.com>
2022-07-31 21:40:51 +0900

    Create PrintMenu.java

---
+++ 자판기/PrintMenu.java
@@ -0,0 +1,16 @@
+public class PrintMenu {
+    public static void printMenu() {
+        System.out.println("따뜻한 음료");
+        System.out.println("1. 아메리카노 (1,200원)");
+        System.out.println("2. 카페라떼 (1,700원)");
+        System.out.println("3. 모카치노 (1,700원)");
+        System.out.println("4. 핫초코 (1,500원)");
+        System.out.println("5. 티백 차 (1,500원)");
+        System.out.println();
+        System.out.println("차가운 음료");
+        System.out.println("6. 아이스 아메리카노 (1,700원)");
+        System.out.println("7. 아이스 초코 (1,700원)");
+        System.out.println("8. 아이스 카페라떼 (1,700원)");
+        System.out.println("9. 복숭아 아이스티 (1,700원)");
+    }
+}
