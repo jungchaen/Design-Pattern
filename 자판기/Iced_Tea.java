248a346d58ce3ba3643923aad891a220cf1c3c9c
jungchuen <50093044+jungchuen@users.noreply.github.com>
2022-07-31 21:39:46 +0900

    Create Iced_Tea.java

---
+++ 자판기/Iced_Tea.java
@@ -0,0 +1,9 @@
+public class Iced_Tea extends Beverage {
+    public Iced_Tea(String beverageName, Hot_Or_Iced hot_ice) {
+        super(beverageName, hot_ice);
+    }
+
+    public int cost() {
+        return cost = 1700;
+    }
+}
