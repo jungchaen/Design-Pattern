b8d7134bfc90a84780844c65cfbc51676a3bac39
jungchuen <50093044+jungchuen@users.noreply.github.com>
2022-07-31 21:41:13 +0900

    Create Teabag.java

---
+++ 자판기/Teabag.java
@@ -0,0 +1,9 @@
+public class Teabag extends Beverage {
+    public Teabag(String beverageName, Hot_Or_Iced hot_ice) {
+        super(beverageName, hot_ice);
+    }
+
+    public int cost() {
+        return cost = 1500;
+    }
+}
