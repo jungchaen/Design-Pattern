600cc1aee7577d64dc31bf657226f927b08979b5
jungchuen <50093044+jungchuen@users.noreply.github.com>
2022-07-31 21:39:58 +0900

    Create Mochaccino.java

---
+++ 자판기/Mochaccino.java
@@ -0,0 +1,9 @@
+public class Mochaccino extends Beverage {
+    public Mochaccino(String beverageName, Hot_Or_Iced hot_ice) {
+        super(beverageName, hot_ice);
+    }
+
+    public int cost() {
+        return cost = 1700;
+    }
+}
