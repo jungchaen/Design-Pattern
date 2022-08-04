b5c89b0ca0e50b924153f69b034188d437383b03
jungchuen <50093044+jungchuen@users.noreply.github.com>
2022-08-05 08:46:41 +0900

    Update PostfixParser.java

--- 인터프리터_사칙연산/PostfixParser.java
+++ 인터프리터_사칙연산/PostfixParser.java
@@ -1,38 +1,59 @@
 import java.util.Stack;
 
 public class PostfixParser {
-    private static int priority(char c) {
-        if (c == '*' || c == '/')
+    private static int priority(String str) {
+        if (str.equals(new String("*")) || str.equals(new String("/")))
             return 2;
-        if (c == '+' || c == '-')
+        if (str.equals("+") || str.equals(new String("-")))
             return 1;
         return 0;
     }
 
     public static String reversePolish(String syntax) {
         char[] charArray = syntax.toCharArray();
-        Stack<Character> stack = new Stack<>();
+        Stack<String> stack = new Stack<>();
         String postFix = "";
+        String temp = "";
 
         for (char c : charArray) {
-            if (c == '(')
-                stack.push(c);
-            else if (c == ')') {
-                while (!stack.isEmpty() && stack.peek() != '(') {
-                    postFix += (char) stack.pop() + " ";
+            if (c == '(') {
+                stack.push(String.valueOf(c));
+
+                if (temp != "") {
+                    postFix += temp + " ";
+                    temp = "";
                 }
-                stack.pop();
+            } else if (c == ')') {
+                if (temp != "") {
+                    postFix += temp + " ";
+                    temp = "";
+                }
+                while (!stack.isEmpty() && !stack.peek().equals(new String("("))) {
+                    postFix += stack.pop() + " ";
+                }
+
+                if (!stack.empty()) {
+                    stack.pop();
+                }
             } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-                while (!stack.isEmpty() && priority((char) stack.peek()) >= priority(c)) {
-                    postFix += (char) stack.pop() + " ";
+                if (temp != null) {
+                    postFix += temp + " ";
+                    temp = "";
                 }
-                stack.push(c);
-            } else if (c >= '0' && c <= '9')
-                postFix += c + " ";
+                while (!stack.isEmpty() && priority(stack.peek()) >= priority(String.valueOf(c))) {
+                    postFix += stack.pop() + " ";
+                }
+                stack.push(String.valueOf(c));
+            } else if (c >= '0' && c <= '9') {
+                temp += c;
+            }
+        }
+        if (temp != "") {
+            postFix += temp + " ";
         }
 
         while (!stack.isEmpty())
-            postFix += (char) stack.pop() + " ";
+            postFix += stack.pop() + " ";
 
         return postFix = postFix.trim();
     }
